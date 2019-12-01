50a92092198d08309645b5454f3417d7abf8fee6
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-12-01 19:13:12 +0330

    Add testcase

---
+++ Phase 2/testcases/2.sol
@@ -0,0 +1,142 @@
+Program
+ActorDeclaration
+Identifier test
+Identifier test3
+VarDeclaration
+Identifier t1
+VarDeclaration
+Identifier t3
+VarDeclaration
+Identifier ty7
+VarDeclaration
+Identifier x
+VarDeclaration
+Identifier y
+VarDeclaration
+Identifier z
+InitHandlerDeclaration
+Identifier initial
+VarDeclaration
+Identifier t
+VarDeclaration
+Identifier a
+VarDeclaration
+Identifier o
+Assign
+ActorVarAccess
+Self
+Identifier x
+Identifier t
+Assign
+ActorVarAccess
+Self
+Identifier y
+Identifier o
+Assign
+ActorVarAccess
+Self
+Identifier z
+Identifier a
+MsgHandlerDeclaration
+Identifier random
+VarDeclaration
+Identifier i
+VarDeclaration
+Identifier j
+VarDeclaration
+Identifier sum
+For
+Assign
+Identifier i
+IntValue 0
+BinaryExpression lt
+Identifier i
+IntValue 7
+Assign
+Identifier i
+BinaryExpression add
+Identifier i
+IntValue 1
+Block
+Conditional
+BinaryExpression eq
+Identifier i
+IntValue 5
+Block
+Print
+StringValue "hello wold\n"
+For
+Assign
+Identifier j
+IntValue 0
+BinaryExpression lt
+Identifier j
+Identifier i
+Assign
+Identifier j
+BinaryExpression add
+Identifier j
+IntValue 1
+Block
+Assign
+Identifier sum
+BinaryExpression add
+Identifier sum
+BinaryExpression mult
+Identifier i
+Identifier j
+MsgHandlerDeclaration
+Identifier call
+VarDeclaration
+Identifier i
+Assign
+Identifier i
+ActorVarAccess
+Self
+Identifier x
+MsgHandlerCall
+Sender
+Identifier test
+BinaryExpression mult
+Identifier u
+IntValue 7
+Conditional
+BinaryExpression eq
+Identifier t
+Identifier u
+For
+Assign
+Identifier i
+IntValue 0
+BinaryExpression neq
+Identifier i
+Identifier k
+Assign
+Identifier k
+BinaryExpression add
+Identifier u
+IntValue 1
+Print
+StringValue "hello"
+Conditional
+Identifier t
+Conditional
+IntValue 763
+Assign
+Identifier te
+IntValue 8
+Assign
+Identifier te
+IntValue 9
+Assign
+Identifier y
+IntValue 0
+Main
+ActorInstantiation
+Identifier a1
+Identifier a2
+IntValue 0
+ActorInstantiation
+Identifier a2
+Identifier a1
+IntValue 1
