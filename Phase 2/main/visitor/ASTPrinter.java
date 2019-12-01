41578dfde1d34e41962fd53a6ab82f69639df4a7
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-12-01 19:13:45 +0330

    Add visitor functions

--- Phase 2/main/visitor/ASTPrinter.java
+++ Phase 2/main/visitor/ASTPrinter.java
@@ -31,6 +31,8 @@ public class ASTPrinter implements Visitor {
         if(main != null) {
             main.accept(this);
         }
+
+        return;
     }
 
     @Override
@@ -75,6 +77,8 @@ public class ASTPrinter implements Visitor {
                 varDec.accept(this);
             }
         }
+
+        return;
     }
 
     @Override
@@ -106,110 +110,294 @@ public class ASTPrinter implements Visitor {
                 stmt.accept(this);
             }
         }
+
+        return;
     }
 
     @Override
     public void visit(VarDeclaration varDeclaration) {
         System.out.println(varDeclaration.toString());
+
+        Identifier id = varDeclaration.getIdentifier();
+        if(id != null) {
+            id.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(Main mainActors) {
         System.out.println(mainActors.toString());
+
+        ArrayList<ActorInstantiation> actors = mainActors.getMainActors();
+        if(actors != null) {
+            for (ActorInstantiation actor : actors) {
+                actor.accept(this);
+            }
+        }
+
+        return;
     }
 
     @Override
     public void visit(ActorInstantiation actorInstantiation) {
         System.out.println(actorInstantiation.toString());
+
+        Identifier id = varDeclaration.getIdentifier();
+        if(id != null) {
+            id.accept(this);
+        }
+
+        ArrayList<Identifier> knownActors = actorInstantiation.getKnownActors();
+        if(knownActors != null) {
+            for (Identifier actor : knownActors) {
+                actor.accept(this);
+            }
+        }
+
+        ArrayList<Expression> initArgs = actorInstantiation.getInitArgs();
+        if(initArgs != null) {
+            for (Expression arg : initArgs) {
+                arg.accept(this);
+            }
+        }
+
+        return;
     }
 
     @Override
     public void visit(UnaryExpression unaryExpression) {
         System.out.println(unaryExpression.toString());
+
+        Expression operand = unaryExpression.getOperand();
+        if(operand != null) {
+            operand.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(BinaryExpression binaryExpression) {
         System.out.println(binaryExpression.toString());
+
+        Expression leftOperand = binaryExpression.getLeft();
+        if(leftOperand != null) {
+            leftOperand.accept(this);
+        }
+
+        Expression rightOperand = binaryExpression.getRight();
+        if(rightOperand != null) {
+            rightOperand.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(ArrayCall arrayCall) {
         System.out.println(arrayCall.toString());
+
+        Expression arrayInstance = arrayCall.getArrayInstance();
+        if(arrayInstance != null) {
+            arrayInstance.accept(this);
+        }
+
+        Expression index = arrayCall.getIndex();
+        if(index != null) {
+            index.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(ActorVarAccess actorVarAccess) {
         System.out.println(actorVarAccess.toString());
+
+        Identifier varName = actorVarAccess.getVariable();
+        if(varName != null) {
+            varName.accept(this);
+        }
+
+        Self self = actorVarAccess.getSelf();
+        if(self != null) {
+            self.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(Identifier identifier) {
         System.out.println(identifier.toString());
+
+        return;
     }
 
     @Override
     public void visit(Self self) {
         System.out.println(self.toString());
+
+        return;
     }
 
     @Override
     public void visit(Sender sender) {
         System.out.println(sender.toString());
+
+        return;
     }
 
     @Override
     public void visit(BooleanValue value) {
         System.out.println(value.toString());
+
+        return;
     }
 
     @Override
     public void visit(IntValue value) {
         System.out.println(value.toString());
+
+        return;
     }
 
     @Override
     public void visit(StringValue value) {
         System.out.println(value.toString());
+
+        return;
     }
 
     @Override
     public void visit(Block block) {
         System.out.println(block.toString());
+
+        ArrayList<Statement> stmts = block.getStatements();
+        if(stmts != null) {
+            for(Statement stmt : stmts) {
+                stmt.accept(this);
+            }
+        }
+
+        return;
     }
 
     @Override
     public void visit(Conditional conditional) {
         System.out.println(conditional.toString());
+
+        Expression expr = conditional.getExpression();
+        if(expr != null) {
+            expr.accept(this);
+        }
+
+        Statement thenBody = conditional.getThenBody();
+        if(thenBody != null) {
+            thenBody.accept(this);
+        }
+
+        Statement elseBody = conditional.getElseBody();
+        if(elseBody != null) {
+            elseBody.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(For loop) {
         System.out.println(loop.toString());
+
+        Assign initialize = loop.getInitialize();
+        if(initialize != null) {
+            initialize.accept(this);
+        }
+
+        Expression condition = loop.getCondition();
+        if(condition != null) {
+            condition.accept(this);
+        }
+
+        Assign update = loop.getUpdate();
+        if(update != null) {
+            update.accept(this);
+        }
+
+        Statement body = loop.getBody();
+        if(body != null) {
+            body.accept(this);
+        }
+
+
+        return;
     }
 
     @Override
     public void visit(Break breakLoop) {
         System.out.println(breakLoop.toString());
+
+        return;
     }
 
     @Override
     public void visit(Continue continueLoop) {
         System.out.println(continueLoop.toString());
+
+        return;
     }
 
     @Override
     public void visit(MsgHandlerCall msgHandlerCall) {
         System.out.println(msgHandlerCall.toString());
+        Expression instance = msgHandlerCall.getInstance();
+        if(instance != null) {
+            instance.accept(this);
+        }
+
+        Identifier msgHandlerName = msgHandlerCall.getMsgHandlerName();
+        if(msgHandlerName != null) {
+            msgHandlerName.accept(this);
+        }
+
+        ArrayList<Expression> args = msgHandlerCall.getArgs();
+        if(args != null) {
+            for(Expression arg : args) {
+                arg.accept(this);
+            }
+        }
+
+        return;
     }
 
     @Override
     public void visit(Print print) {
         System.out.println(print.toString());
+
+        Expression arg = print.getArg();
+        if(arg != null) {
+            arg.accept(this);
+        }
+
+        return;
     }
 
     @Override
     public void visit(Assign assign) {
         System.out.println(assign.toString());
+
+        Expression lValue = assign.getlValue();
+        if(lValue != null){
+            lValue.accept(null);
+        }
+
+        Expression rValue = assign.getrValue();
+        if(rValue != null){
+            rValue.accept(null);
+        }
+
+        return;
     }
 }
